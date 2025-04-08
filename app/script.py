5e08e7dfbb5b529f232eb56115d5c5595b351d0e
kill Bill <60369904+olegenka@users.noreply.github.com>
2025-04-08 11:14:31 +0300

    Initial commit

---
+++ app/script.py
@@ -0,0 +1,20 @@
+﻿import psycopg2
+import time
+
+time.sleep(5)  # Ждем инициализацию БД
+
+conn = psycopg2.connect(
+    dbname="testdb",
+    user="user",
+    password="password",
+    host="database"
+)
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM messages;")
+print("Data from database:")
+for row in cur.fetchall():
+    print(row)
+
+cur.close()
+conn.close()
---
+++ db/init.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE IF NOT EXISTS messages (
+    id SERIAL PRIMARY KEY,
+    text VARCHAR(255) NOT NULL
+);
+
+INSERT INTO messages(text) VALUES ('Hello from Database!');
